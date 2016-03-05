d06f0b6bebe0a82fe96302c3c26005a6e75d1ce8
Cassity <y.tassinSD@gmail.com>
2016-03-05 02:13:11 +0100

    no message

---
+++ AudioScriptingLearning/Assets/CharacterAudioManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterAudioManager : MonoBehaviour {
+    public AudioClip FootstepsSound;
+    private bool IsWalking;
+    private bool IsRunning;
+    public float OverallSpeed;
+    private AudioSource FsSource;
+    private Rigidbody Body;
+    
+	// Use this for initialization
+	void Awake () {
+        FsSource = GetComponent<AudioSource>();
+        Body = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        OverallSpeed = Body.velocity.magnitude;
+	
+        if(OverallSpeed > 0 & OverallSpeed < 1)
+        {
+            IsWalking = true;
+            IsRunning = false;
+          //  print("Walking");
+        }
+        else if (OverallSpeed > 1)
+        {
+            IsWalking = false;
+            IsRunning = true;
+           // print("Running");
+        }
+
+        
+	}
+
+    function  OnCollisionEnter () {
+
+        print("FS");
+    }
+    
+}
+ 
