c42539a4bb3b0cdf693b934c3b5377ffd15410b4
Cassity <y.tassinSD@gmail.com>
2016-03-06 17:34:43 +0100

    FS Working, Next Step: Material

--- AudioScriptingLearning/Assets/CharacterAudioManager.cs
+++ AudioScriptingLearning/Assets/CharacterAudioManager.cs
@@ -2,52 +2,76 @@
 using System.Collections;
 
 public class CharacterAudioManager : MonoBehaviour {
-    public AudioClip FootstepsSound;
+    public AudioClip[] WalkingFootstepsSounds;
+	public AudioClip[] RunningFootstepsSounds;
+
+	public float OverallSpeed;
+
+
     private bool IsWalking;
-    private bool IsRunning;
-    public float OverallSpeed;
+    private bool IsRunning; 
     private AudioSource FsSource;
     private Rigidbody Body;
+	private float MinVelocity = 0;
+	private float MaxVelocity = 1;
+	private float CurrentVelocity;
+
     
 	// Use this for initialization
 	void Awake () {
         FsSource = GetComponent<AudioSource>();
         Body = GetComponent<Rigidbody>();
+	
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		
+        CurrentVelocity = Body.velocity.magnitude;
+		OverallSpeed = Mathf.Lerp(MinVelocity,MaxVelocity,CurrentVelocity);
+		FsSource.volume = OverallSpeed;
 
-        OverallSpeed = Body.velocity.magnitude;
+		//Check if Running or Walking
 	
-        if(OverallSpeed > 0 & OverallSpeed < 1)
+        if(OverallSpeed > 0 & OverallSpeed != 1)
         {
             IsWalking = true;
             IsRunning = false;
           //  print("Walking");
         }
-        else if (OverallSpeed > 1)
+        else if (OverallSpeed == 1)
         {
             IsWalking = false;
             IsRunning = true;
            // print("Running");
         }
-		else if (OverallSpeed < 0.004){
+		else if (OverallSpeed < 0.5){
 			IsWalking = false;
 			IsRunning = false;
 
         
 		}
 	}
+	
 
-	void PlayFS () {
-		if (IsWalking = true){
+		//Play Footsteps Sounds
+		void PlayFSSound () {
+			
+		if (IsWalking = true & IsRunning == false ){
+			//FsSource.clip = WalkingFootstepsSounds;
+			FsSource.PlayOneShot(WalkingFootstepsSounds[Random.Range(0,WalkingFootstepsSounds.Length)][Random.Range(0,clipStorage.Length)]);
+			FsSource.Play();
 		print("Walk");
+
     	}
-		else if (IsRunning = true ){
+		else if (IsRunning = true & IsWalking == false){
+			//FsSource.clip = RunningFootstepsSound;
+			FsSource.PlayOneShot(RunningFootstepsSounds[Random.Range(0,RunningFootstepsSounds.Length)]);
 		print ("Run");
+		
 		}
-		else if (IsRunning == false & IsWalking == false ){
+		else if (IsRunning == false ){
 			print ("NoSound");
 		}
 		else {
